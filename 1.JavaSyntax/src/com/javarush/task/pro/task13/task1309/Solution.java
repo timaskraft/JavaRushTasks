fe717c6deb35bc7f9572ed9004cb3857746c3607
timaskraft <timaskraft@gmail.com>
2024-05-09 09:14:05 +0300

    another next tasks...

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1309/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.pro.task13.task1309;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Успеваемость студентов
@@ -16,5 +17,10 @@ public class Solution {
 
     public static void addStudents() {
         //напишите тут ваш код
+        grades.put("Ivanov Ivan",5.0);
+        grades.put("Sidorov Ivan",4.3);
+        grades.put("Petrov Petya",3.4);
+        grades.put("Azarova Anna",4.8);
+        grades.put("Roberst Julia",4.2);
     }
 }
